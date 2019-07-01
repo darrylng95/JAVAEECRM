8d0f0d3fc3b8fca24b4deb91830477e34a2f6718
Darryl Ng <ngziliang95@gmail.com>
2019-07-01 23:23:18 +0800

    Added Remaining Entities

--- Downloads/EBayProj/EBayLibrary/src/entity/ItemEntity.java
+++ Downloads/EBayProj/EBayLibrary/src/entity/ItemEntity.java
@@ -1,14 +1,140 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author darryl
  */
+@Entity
 public class ItemEntity {
+     private static final long serialVersionUID = 1L;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(nullable = false)
+     private Long itemId;
+     
+     private String name;
+     private String description;
+     private int quantityAvail;
+     private String category;
+     private int price;
+     private byte[] pic;
+     
+     @OneToMany(mappedBy = "item")
+     private List<OrderEntity> orderEntities;
+     
+     public ItemEntity(){
+         
+     }
+     
+     public ItemEntity(String name, String description, int quantityAvail, String category, int price, byte[] pic){
+         this.name = name;
+         this.description = description;
+         this.quantityAvail = quantityAvail;
+         this.category = category;
+         this.price = price;
+         this.pic = pic;
+     }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantityAvail() {
+        return quantityAvail;
+    }
+
+    public void setQuantityAvail(int quantityAvail) {
+        this.quantityAvail = quantityAvail;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public byte[] getPic() {
+        return pic;
+    }
+
+    public void setPic(byte[] pic) {
+        this.pic = pic;
+    }
+
+    public List<OrderEntity> getOrderEntities() {
+        return orderEntities;
+    }
+
+    public void setOrderEntities(List<OrderEntity> orderEntities) {
+        this.orderEntities = orderEntities;
+    }
+     
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (itemId != null ? itemId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ItemEntity)) {
+            return false;
+        }
+        ItemEntity other = (ItemEntity) object;
+        if ((this.itemId == null && other.itemId != null) || (this.itemId != null && !this.itemId.equals(other.itemId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Item[ id=" + itemId + " ]";
+    }
+
     
 }
--- Downloads/EBayProj/EBayLibrary/src/entity/OrderEntity.java
+++ Downloads/EBayProj/EBayLibrary/src/entity/OrderEntity.java
@@ -1,14 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
+import java.io.Serializable;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 /**
  *
  * @author darryl
  */
-public class OrderEntity {
+@Entity
+public class OrderEntity implements Serializable {
+  private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (nullable = false)
+    private Long orderId;
+    
+    private int orderQuantity;
+    private String orderStatus = "In Cart";
+    private String orderFeedback;
+    
+    @ManyToOne // mapping to UserEntity
+    private UserEntity user;
+    
+    @ManyToOne // to Item Entity
+    private ItemEntity item;
+    
+    public OrderEntity(){
+        
+    }
+    
+    public OrderEntity(int quantity){
+        this.orderQuantity = quantity;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getOrderQuantity() {
+        return orderQuantity;
+    }
+
+    public void setOrderQuantity(int orderQuantity) {
+        this.orderQuantity = orderQuantity;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderFeedback() {
+        return orderFeedback;
+    }
+
+    public void setOrderFeedback(String orderFeedback) {
+        this.orderFeedback = orderFeedback;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public ItemEntity getItem() {
+        return item;
+    }
+
+    public void setItem(ItemEntity item) {
+        this.item = item;
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (orderId != null ? orderId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OrderEntity)) {
+            return false;
+        }
+        OrderEntity other = (OrderEntity) object;
+        if ((this.orderId == null && other.orderId != null) || (this.orderId != null && !this.orderId.equals(other.orderId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Contact[ id=" + orderId + " ]";
+    }
     
 }
