b5d089b06e507dd409e122d1bd907bb8c59dc142
Darryl Ng <ngziliang95@gmail.com>
2019-07-01 22:58:04 +0800

    Added User Entity

--- Downloads/EBayProj/EBayLibrary/src/entity/UserEntity.java
+++ Downloads/EBayProj/EBayLibrary/src/entity/UserEntity.java
@@ -1,11 +1,177 @@
 /*
 IS3106 Practice Material
  */
+
 package entity;
 /**
  *
  * @author darryl
  */
-public class UserEntity {
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class UserEntity implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (nullable = false)
+    private Long userId;
+    
+    private String name;
+    private String email;
+    private String address;
+    private int phoneNumber;
+    private int activationStatus = 1;
+    private int adminStatus = 0;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    
+    @OneToMany (mappedBy = "user") // User is owned by OrderEntity bi directional
+    private List<OrderEntity> orderEntities;
+    
+    @OneToMany // uni direction to Item
+    private List<ItemEntity> itemEntities;
+    
+    public UserEntity(){
+    
+    } // Default Constructor
+    
+    public UserEntity (String name, String email, String address, int phoneNumber, int adminStatus, String username, String password, List<ItemEntity> itemEntities){
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.adminStatus = adminStatus;
+        this.username= username;
+        this.password = password;
+        this.itemEntities = itemEntities;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getActivationStatus() {
+        return activationStatus;
+    }
+
+    public void setActivationStatus(int activationStatus) {
+        this.activationStatus = activationStatus;
+    }
+
+    public int getAdminStatus() {
+        return adminStatus;
+    }
+
+    public void setAdminStatus(int adminStatus) {
+        this.adminStatus = adminStatus;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<OrderEntity> getOrderEntities() {
+        return orderEntities;
+    }
+
+    public void setOrderEntities(List<OrderEntity> orderEntities) {
+        this.orderEntities = orderEntities;
+    }
+
+    public List<ItemEntity> getItemEntities() {
+        return itemEntities;
+    }
+
+    public void setItemEntities(List<ItemEntity> itemEntities) {
+        this.itemEntities = itemEntities;
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userId != null ? userId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UserEntity)) {
+            return false;
+        }
+        UserEntity other = (UserEntity) object;
+        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Customer[ id=" + userId + " ]";
+    }
     
 }
