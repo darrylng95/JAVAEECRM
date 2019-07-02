ba87d14688b00e1aa2a16a60f59cbbd482b7351c
Darryl Ng <ngziliang95@gmail.com>
2019-07-03 00:28:28 +0800

    CRM Lab

---
+++ Downloads/CRMLab/CRMLab/CRMLab-ejb/src/java/session/CustomerSessionLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.Contact;
+import entity.Customer;
+import entity.Field;
+import java.util.List;
+import java.util.Set;
+import javax.ejb.Local;
+import error.NoResultException;
+
+/**
+ *
+ * @author darryl
+ */
+@Local
+public interface CustomerSessionLocal {
+    public List<Customer> searchCustomers(String name);
+    public Customer getCustomer(Long cId) throws NoResultException;
+    public void createCustomer(Customer c);
+    public void updateCustomer(Customer c) throws NoResultException;
+    public void addContact(Long cId, Contact c) throws NoResultException;
+    public void addField(Long cId, Field f) throws NoResultException;
+    public void deleteContact(Long cId) throws NoResultException;
+    public void deleteField(Long cId, Long fId) throws NoResultException;
+    public void deleteCustomer(Long cId) throws NoResultException;
+    public List<Customer> searchCustomersByContact(Contact c);
+    public List<Customer> searchCustomersByField(Field f);
+    public Set<String> getAllFieldNames();
+}
